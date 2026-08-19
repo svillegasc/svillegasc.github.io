5cf025bcf09e3980ae45decbca11e76d771607b6
santiago-villegas <santivicastro18@gmail.com>
2026-08-18 22:53:11 -0500

    feat: 404 page, wrangler security fix

--- src/data/site.js
+++ src/data/site.js
@@ -1,4 +1,4 @@
 export const SITE_URL = 'https://svillegasc.github.io';
-export const LINKEDIN_URL = 'https://linkedin.com/in/santiagovillegas-castro-06175a9b/';
+export const LINKEDIN_URL = 'https://www.linkedin.com/in/santiago-villegas-castro-06175a9b/';
 export const GITHUB_URL = 'https://github.com/svillegasc';
 export const WEB3FORMS_KEY = 'ba58360c-e987-44fd-ac87-6c5c30f2bd12';
